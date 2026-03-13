0c5c0b766dd5c50e8761e0dbd53ab1e7a31f0f26
Admin <pawneshk22@gmail.com>
2026-03-13 22:38:53 +0530

    feat: profile page - add skills tag-input, map picker for address, hide coordinates, fix volunteer dashboard Link import

--- frontend/src/pages/Dashboards/VolunteerDashboard.jsx
+++ frontend/src/pages/Dashboards/VolunteerDashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { FaCheckCircle, FaMapMarkerAlt, FaStar } from 'react-icons/fa';
 import { useTheme } from '../../context/ThemeContext';
